ac5ba9c331dcf2dc112ab24285a06222ccd6ff3c
JetsadaWijit <jetsadawijit@outlook.com>
2025-10-22 18:53:19 +0700

    feat: Add check license

--- server/papermc/engine/src/main/java/io/github/mcengine/papermc/world/engine/MCEngineWorldPaperMC.java
+++ server/papermc/engine/src/main/java/io/github/mcengine/papermc/world/engine/MCEngineWorldPaperMC.java
@@ -30,9 +30,11 @@ public class MCEngineWorldPaperMC extends JavaPlugin {
             return;
         }
 
-        final String licenseType = getConfig().getString("license", "free");
-        if (!"free".equalsIgnoreCase(licenseType)) {
-            getLogger().warning("License is not 'free'. Disabling MCEngineIdentity.");
+        String license = getConfig().getString("licenses.license", "free"); 
+        if (!license.equalsIgnoreCase("free")) { 
+            getLogger().warning("Plugin is disabled in config.yml.");
+            getLogger().warning("Invalid license.");
+            getLogger().warning("Check license or use \"free\".");
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
